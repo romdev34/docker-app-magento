386ad185aabe670dbf6353b5fedf99e2d3e8c189
Rom Dev <romdev34@gmail.com>
2025-06-21 18:08:45 +0200

    misc

--- system/docker/d-bootstrap-magento.sh
+++ system/docker/d-bootstrap-magento.sh
@@ -6,31 +6,22 @@ set -e
 if [ "$MAGE_MODE" != "developer" ]; then
   (>&2 echo "[*] STARTING MAGENTO PRODUCTION MODE")
 
-  bin/magento maintenance:enable
+  if [ -f "/var/www/app/etc/env.php" ]; then
+    # on redirige le retour de la bin/magento se:db:status dans dev/null pour ne pas l'afficher et on redirige les erreurs dans l'output qui lui meme a été déja redirigé dans une pseudo classe pour ne pas etre affiché
+    until mysql -h"mysql" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
+    # on reidirige le message Waiting for DATABASE to be ready... dans le STDERR et donc afficher l'erreur avec le message
+        (echo >&2 "[!] Waiting for DATABASE to be ready...")
+        sleep 2
+    #
+    done
 
-  composer install \
-  --optimize-autoloader \
-  --no-dev;
+    bin/magento maintenance:enable
 
-  bin/magento setup:upgrade --keep-generated
+    composer install \
+    --optimize-autoloader \
+    --no-dev;
 
-else
-  (>&2 echo "[*] STARTING MAGENTO DEVELOPER MODE")
-  composer install
-fi
-
-if [ -f "/var/www/app/etc/env.php" ]; then
-  # on redirige le retour de la bin/magento se:db:status dans dev/null pour ne pas l'afficher et on redirige les erreurs dans l'output qui lui meme a été déja redirigé dans une pseudo classe pour ne pas etre affiché
-  until mysql -h"mysql" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
-  # on reidirige le message Waiting for DATABASE to be ready... dans le STDERR et donc afficher l'erreur avec le message
-      (echo >&2 "[!] Waiting for DATABASE to be ready...")
-      sleep 2
-  #
-  done
-fi
-
-if [ "$MAGE_MODE" != "developer" ]; then
-
+    bin/magento setup:upgrade --keep-generated
 
     (>&2 echo "[*] Bootstrap COMPILE")
     bin/magento se:di:co
@@ -59,4 +50,9 @@ if [ "$MAGE_MODE" != "developer" ]; then
 
       (>&2 echo "[*] Disabling maintenance mode")
       bin/magento maintenance:disable
+
+  else
+    (>&2 echo "[*] STARTING MAGENTO DEVELOPER MODE")
+    composer install
+  fi
 fi
