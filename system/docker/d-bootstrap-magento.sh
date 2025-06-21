ee94094c6f294c81bd9884568c031ae660ba72b4
Rom Dev <romdev34@gmail.com>
2025-06-21 18:41:41 +0200

    misc

--- system/docker/d-bootstrap-magento.sh
+++ system/docker/d-bootstrap-magento.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
 set -e
 
+PROXY_FILE="/var/www/generated/code/Magento/Framework/App/ResourceConnection/Proxy.php"
+
 (>&2 echo "[*] Bootstrap MAGENTO")
+
 
 if [ "$MAGE_MODE" != "developer" ]; then
   (>&2 echo "[*] STARTING MAGENTO PRODUCTION MODE")
+  composer install \
+  --optimize-autoloader \
+  --no-dev;
 
-    composer install \
-    --optimize-autoloader \
-    --no-dev;
-
+  if [ ! -f "$PROXY_FILE" ]; then
+    echo "[!] Proxy class missing: $PROXY_FILE"
+    echo "[*] Forcing Magento into developer mode to generate missing proxies"
     bin/magento deploy:mode:set developer
-  
+    bin/magento maintenance:enable
+
+    bin/magento setup:di:compile
+    (>&2 echo "[*] Bootstrap DEPLOY STATIC")
+
+    bin/magento \
+    setup:static-content:deploy \
+    --jobs=6 \
+    --force \
+    --strategy=quick \
+    en_US fr_FR
+
+    bin/magento \
+    setup:static-content:deploy \
+    --jobs=6 \
+    --force \
+    --strategy=quick \
+    --area adminhtml \
+    fr_FR
+
+    # Nettoyage final du cache
+    (>&2 echo "[*] Cleaning cache")
+    bin/magento cache:clean
+    bin/magento cache:flush
+  else
     (>&2 echo "[*] Mode maintenance activé")
-    # cette commande implique le di:compile et le deploy static-content
     bin/magento deploy:mode:set production
+    (>&2 echo "[*] Disabling maintenance mode")
+    bin/magento maintenance:disable
+  fi
 
   if [ -f "/var/www/app/etc/env.php" ]; then
     # on redirige le retour de la bin/magento se:db:status dans dev/null pour ne pas l'afficher et on redirige les erreurs dans l'output qui lui meme a été déja redirigé dans une pseudo classe pour ne pas etre affiché
@@ -26,14 +57,6 @@ if [ "$MAGE_MODE" != "developer" ]; then
     done
 
     bin/magento setup:upgrade --keep-generated
-      
-      # Nettoyage final du cache
-      (>&2 echo "[*] Cleaning cache")
-      bin/magento cache:clean
-      bin/magento cache:flush
-
-      (>&2 echo "[*] Disabling maintenance mode")
-      bin/magento maintenance:disable
 
   else
     (>&2 echo "[*] STARTING MAGENTO DEVELOPER MODE")
