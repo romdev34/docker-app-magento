11f41e3a8b0284de7d6ac8a7a1e2e393c193ccc7
Rom Dev <romdev34@gmail.com>
2025-06-21 15:48:15 +0200

    misc

--- system/docker/d-bootstrap-magento.sh
+++ system/docker/d-bootstrap-magento.sh
@@ -12,11 +12,6 @@ set -e
     (>&2 echo "[*] STARTING MAGENTO PRODUCTION MODE")
     bin/magento maintenance:enable
 
-
-    (>&2 echo "[*] Bootstrap COMPILE")
-    bin/magento se:di:co
-
-
   else
   (>&2 echo "[*] STARTING MAGENTO DEVELOPER MODE")
   composer install
@@ -24,7 +19,7 @@ set -e
 
 if [ -f "/var/www/app/etc/env.php" ]; then
 # on redirige le retour de la bin/magento se:db:status dans dev/null pour ne pas l'afficher et on redirige les erreurs dans l'output qui lui meme a été déja redirigé dans une pseudo classe pour ne pas etre affiché
-  until bin/magento setup:db:status >/dev/null 2>&1; do
+until mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
 # on reidirige le message Waiting for DATABASE to be ready... dans le STDERR et donc afficher l'erreur avec le message
     (echo >&2 "[!] Waiting for DATABASE to be ready...")
     sleep 2
@@ -36,7 +31,9 @@ if [ -f "/var/www/app/etc/env.php" ]; then
       (>&2 echo "[*] Bootstrap setup:upgrade")
       bin/magento setup:upgrade
 
-
+    (>&2 echo "[*] Bootstrap COMPILE")
+    bin/magento se:di:co
+    
   # MAINTENANT on peut optimiser l'autoloader
       (>&2 echo "[*] Optimizing autoloader")
       composer dump-autoload --optimize --classmap-authoritative
